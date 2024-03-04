99d63520f2aa15c378dc153c1530636910f386d8
ManuJoker <hubervicemmanuel@gmail.com>
2024-03-04 08:02:05 +0000

    Questionnaire fonctionne

--- questionnaire.py
+++ questionnaire.py
@@ -20,63 +20,35 @@
 #
 
 import json
-
 # I've first to import json files here
 # fichiers json
-file_names = ["animaux_leschats_confirme.json", "animaux_leschats_debutant.json", "animaux_leschats_expert.json", "arts_museedulouvre_confirme.json", "arts_museedulouvre_debutant.json", "arts_museedulouvre_expert.json", "cinema_alien_confirme.json", "cinema_alien_debutant.json", "cinema_alien_expert.json", "cinema_starwars_confirme.json", "cinema_starwars_debutant.json", "cinema_starwars_expert.json"]
-for file_name in file_names:
-    file = open(file_name, "r")
-    json_data = file.read()
-    questionnaire_data = json.loads(json_data)    #  charger en ficher lisible (str)
-    print(questionnaire_data['questions'][2]['choix'])
-    # 'questionnaire_data' contains category, title , questions and difficulty level
-    # 'questions' contains titles and choices
-    # 'choix' contains the choice in question,  and true response
 
-# Choose the category , the title of the category and the difficulty before starting to ask questions
-class Head:
-    def __init__(self, categorie, titre_de_categorie, difficulte):
-        self.categorie = categorie
-        self.titre_de_categorie = titre_de_categorie
-        self.difficulte = difficulte
-
-    # Identifier la catégorie, titre et difficulte grace au fichier data
-
-    def tirer_parametres_de_data(self):
-        parametres = [file_name.replace("_", " ")]  # Tirer les paramètres à travers le nom du fichier
-
-
-
-# Faire des fonctions pour gérer le choix de la catégorie,
-                #    Si possible du titre de catégorie,
-                     #    Et ensuite celle de la difficulté
 
 class Question:
     def __init__(self, titre_de_question, choix, bonne_reponse):
-        self.titre = titre_de_question
+        self.titre_de_question = titre_de_question
         self.choix = choix
         self.bonne_reponse = bonne_reponse
 
 
 
-    def FromData(data):
+    def FromJsonData(data):
         # ....
-        for i in data['questions'] :
-            data_question = data['questions'][i]
-            data_choix = data_question['choix']
-            for c in data_choix:
-                les_choix = []
-                choix = c[0]
-                les_choix.append(choix)  # constituer la liste des choix avant de gérer la bonne réponse
-                if choix[1] == "True":
-                    b_reponse = choix   # identification de la bonne réponse
+
+
+        choix = [i[0] for i in data['choix']]
+
+        [bonne_reponse] = [i[0] for i in data['choix'] if i[1]]  # bonne réponse lorsque il y'a True
+        if len([bonne_reponse]) != 1:
+            return None
 
         # i've to know how to identificate the right answer
-        q = Question(data_question['titre'], les_choix, b_reponse)
+        q = Question(data['titre'], choix, bonne_reponse)
         return q
 
     def poser(self):
-        print("QUESTION")
+
+        print(f"QUESTION n°{len(questions)}")
         print("  " + self.titre_de_question)
         for i in range(len(self.choix)):
             print("  ", i+1, "-", self.choix[i])
@@ -104,13 +76,25 @@ class Question:
         except:
             print("ERREUR : Veuillez rentrer uniquement des chiffres")
         return Question.demander_reponse_numerique_utlisateur(min, max)
-    
-class Questionnaire:
-    def __init__(self, questions):
+
+
+# Choose the category , the title of the category and the difficulty before starting to ask questions
+
+class Questionnaire:  # Reconstituer la classe Questionnaire
+
+    def __init__(self, categorie, titre_de_categorie, difficulte, questions):
+        self.categorie = categorie
+        self.titre_de_categorie = titre_de_categorie
+        self.difficulte = difficulte
         self.questions = questions
+
 
     def lancer(self):
         score = 0
+        print(f"CATEGORIE: {self.categorie}")
+        print(f"Titre de catégorie: {self.titre_de_categorie}")
+        print(f"Difficulté: {self.difficulte}")
+        print(f"Nombre de question: {len(questions)}")
         for question in self.questions:
             if question.poser():
                 score += 1
@@ -118,26 +102,71 @@ class Questionnaire:
         return score
 
 
-"""questionnaire = (
-    ("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
-    ("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-    ("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
-                )
+def file_parameter(file_name):
+    file = open(file_name, "r")
+    json_data = file.read()
+    file.close()
+    questionnaire_data = json.loads(json_data)  # charger en ficher lisible (str)
+    categorie = questionnaire_data['categorie']
+    titre_de_categorie = questionnaire_data['titre']
+    difficulte = questionnaire_data['difficulte']
+    question_data_question = questionnaire_data['questions']
+    return categorie, titre_de_categorie, difficulte, question_data_question
 
-lancer_questionnaire(questionnaire)"""
 
-# q1 = Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris")
-# q1.poser()
+ # 'questionnaire_data' contains category, title , questions and difficulty level
+    # 'questions' contains titles and choices
+    # 'choix' contains the choice in question,  and true response
 
-# data = (("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris", "Quelle est la capitale de la France ?")
-# q = Question.FromData(data)
-# print(q.__dict__)
+    # Identifier la catégorie, titre et difficulte grace au fichier data
 
-"""Questionnaire(
-    (
-    Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
-    Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-    Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
-    )
-).lancer()"""
+# Faire des fonctions pour gérer le choix de la catégorie,
+#    Si possible du titre de catégorie,
+#    Et ensuite celle de la difficulté
+
+# First of all select the category, category title and difficulty by user
+
+
+file_names = ["animaux_leschats_confirme.json", "animaux_leschats_debutant.json", "animaux_leschats_expert.json",
+              "arts_museedulouvre_confirme.json", "arts_museedulouvre_debutant.json", "arts_museedulouvre_expert.json",
+              "cinema_alien_confirme.json", "cinema_alien_debutant.json", "cinema_alien_expert.json",
+              "cinema_starwars_confirme.json", "cinema_starwars_debutant.json", "cinema_starwars_expert.json"]
+
+
+print("Voulez-vous que votre questionnaire soit de :")
+
+i = 0   # Gérer l'affichage des numéros
+for file_name in file_names:
+    i += 1     # Implémenter la valeur de i de +1 à chaque boucle
+    categorie, titre_de_categorie, difficulte, question_data_question= file_parameter(file_name)
+    print(f"{i}) Catégorie: '{categorie}'      ------ Titre: '{titre_de_categorie}'     ------     Difficulté: '{difficulte}'  ")
+
+
+choix_joueur = int(input("Votre préférence:"))
+
+questions = []
+file_choice = file_names[choix_joueur-1]     # Choix de fichier du user
+
+categorie_c, titre_de_categorie_c, difficulte_c, question_data_question_c = file_parameter(file_choice) # Paramètres de catégorie choisie du user
+
+
+
+#print(titre_de_categorie)
+for i in range(0, len(question_data_question_c)):   # Poser pr toutes les questions
+    q = Question.FromJsonData(question_data_question_c[i])
+    questions.append(q)
+#q.poser()
+Questionnaire(categorie_c, titre_de_categorie_c, difficulte_c, questions).lancer()
+
+
+
+
+
+
+
+
+
+
+
+
 
